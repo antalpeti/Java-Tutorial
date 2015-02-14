ca58f9ed0829883decbb41da1e488c33952d9ca3
Peti <antalpeti@users.noreply.github.com>
2015-02-14 19:40:20 +0100

    Added example of simple class declaration with fields and methods.

---
+++ src/tutorialspoint/objectsandclasses/Dog.java
@@ -0,0 +1,13 @@
+package tutorialspoint.objectsandclasses;
+
+public class Dog {
+  String breed;
+  int age;
+  String color;
+
+  void barking() {}
+
+  void hungry() {}
+
+  void sleeping() {}
+}
