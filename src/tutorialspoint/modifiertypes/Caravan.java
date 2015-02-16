b492bd58993fdfd86526aa5f7840736e42a9b846
Peti <antalpeti@users.noreply.github.com>
2015-02-16 15:13:32 +0100

    Added abstract modifier example.

---
+++ src/tutorialspoint/modifiertypes/Caravan.java
@@ -0,0 +1,11 @@
+package tutorialspoint.modifiertypes;
+
+abstract class Caravan {
+  private double price;
+  private String model;
+  private String year;
+
+  public abstract void goFast(); // an abstract method
+
+  public abstract void changeColor();
+}
