ea405e702b8d6a5b0016d82bd75ce42dee32fe48
Peti <antalpeti@users.noreply.github.com>
2015-02-18 07:42:12 +0100

    Refactor. Added examples about xxxValue() and compareTo().

--- src/tutorialspoint/numbers/Test.java
+++ src/tutorialspoint/numbers/Test.java
@@ -1,10 +1,37 @@
 package tutorialspoint.numbers;
 
 public class Test {
-
-  public static void main(String args[]) {
+  private static void execuetBoxing() {
     Integer x = 5; // boxes int to an Integer object
     x = x + 10; // unboxes the Integer to a int
     System.out.println(x);
+  }
+
+  private static void executeXXXValue() {
+    Integer x = 5;
+    // Returns byte primitive data type
+    System.out.println(x.byteValue());
+
+    // Returns double primitive data type
+    System.out.println(x.doubleValue());
+
+    // Returns long primitive data type
+    System.out.println(x.longValue());
+  }
+
+  private static void executeCompareTo() {
+    Integer x = 5;
+    System.out.println(x.compareTo(3));
+    System.out.println(x.compareTo(5));
+    System.out.println(x.compareTo(8));
+  }
+
+  public static void main(String args[]) {
+    System.out.println("Boxing and Unboxing");
+    execuetBoxing();
+    System.out.println("\nxxxValue()");
+    executeXXXValue();
+    System.out.println("\ncompareTo()");
+    executeCompareTo();
   }
 }
