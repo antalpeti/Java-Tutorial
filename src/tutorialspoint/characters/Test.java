8d857bdd77adf7a815390e1a4bef3a4eabd9110f
Peti <antalpeti@users.noreply.github.com>
2015-02-20 19:21:20 +0100

    Refactor. Added examples about isLetter(), isDigit(), isWhitespace(), isUpperCase(), isLowerCase(), toUpperCase(), toLowerCase() and toString().

--- src/tutorialspoint/characters/Test.java
+++ src/tutorialspoint/characters/Test.java
@@ -1,6 +1,59 @@
 package tutorialspoint.characters;
 
 public class Test {
+
+  private static Character unbox(char ch) {
+    return new Character(ch);
+  }
+
+  private static void executeIsLetter() {
+    System.out.println(Character.isLetter('c'));
+    System.out.println(Character.isLetter('5'));
+  }
+
+  private static void executeIsDigit() {
+    System.out.println(Character.isDigit('c'));
+    System.out.println(Character.isDigit('5'));
+  }
+
+  private static void executeIsWhitespace() {
+    System.out.println(Character.isWhitespace('c'));
+    System.out.println(Character.isWhitespace(' '));
+    System.out.println(Character.isWhitespace('\n'));
+    System.out.println(Character.isWhitespace('\t'));
+    System.out.println(Character.isWhitespace('\b'));
+    System.out.println(Character.isWhitespace('\r'));
+  }
+
+  private static void executeIsUpperCase() {
+    System.out.println(Character.isUpperCase('c'));
+    System.out.println(Character.isUpperCase('C'));
+    System.out.println(Character.isUpperCase('\n'));
+    System.out.println(Character.isUpperCase('\t'));
+  }
+
+  private static void executeIsLowerCase() {
+    System.out.println(Character.isLowerCase('c'));
+    System.out.println(Character.isLowerCase('C'));
+    System.out.println(Character.isLowerCase('\n'));
+    System.out.println(Character.isLowerCase('\t'));
+  }
+
+  private static void executeToUpperCase() {
+    System.out.println(Character.toUpperCase('c'));
+    System.out.println(Character.toUpperCase('C'));
+  }
+
+  private static void executeToLowerCase() {
+    System.out.println(Character.toLowerCase('c'));
+    System.out.println(Character.toLowerCase('C'));
+  }
+
+  private static void executeToString() {
+    System.out.println(Character.toString('c'));
+    System.out.println(Character.toString('C'));
+  }
+
   public static void main(String[] args) {
 
     // Unicode for uppercase Greek omega character
@@ -30,9 +83,23 @@ public class Test {
     }
 
     System.out.println("\nShe said \"Hello!\" to me.");
+
+    System.out.println("\nisLetter()");
+    executeIsLetter();
+    System.out.println("\nisDigit()");
+    executeIsDigit();
+    System.out.println("\nisWhitespace()");
+    executeIsWhitespace();
+    System.out.println("\nisUpperCase()");
+    executeIsUpperCase();
+    System.out.println("\nisLowerCase()");
+    executeIsLowerCase();
+    System.out.println("\ntoUpperCase()");
+    executeToUpperCase();
+    System.out.println("\ntoLowerCase()");
+    executeToLowerCase();
+    System.out.println("\ntoString()");
+    executeToString();
   }
 
-  private static Character unbox(char ch) {
-    return new Character(ch);
-  }
 }
