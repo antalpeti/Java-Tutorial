cf623204813583842d93f86f95528d1b347c86c5
Peti <antalpeti@users.noreply.github.com>
2015-02-20 18:57:51 +0100

    Added examples about tan(), asin(), acos(), atan(), atan2(), toDegrees(), toRadians() and random().

--- src/tutorialspoint/numbers/Test.java
+++ src/tutorialspoint/numbers/Test.java
@@ -171,6 +171,70 @@ public class Test {
     System.out.format("The cosine of %.1f degrees is %.4f%n", degrees, Math.cos(radians));
   }
 
+  private static void executeTan() {
+    double degrees = 45.0;
+    double radians = Math.toRadians(degrees);
+
+    System.out.format("The value of pi is %.4f%n", Math.PI);
+    System.out.format("The tangent of %.1f degrees is %.4f%n", degrees, Math.tan(radians));
+  }
+
+  private static void executeAsin() {
+    double degrees = 45.0;
+    double radians = Math.toRadians(degrees);
+
+    System.out.format("The value of pi is %.4f%n", Math.PI);
+    System.out.format("The arcsine of %.4f is %.4f degrees %n", Math.sin(radians),
+        Math.toDegrees(Math.asin(Math.sin(radians))));
+  }
+
+  private static void executeAcos() {
+    double degrees = 45.0;
+    double radians = Math.toRadians(degrees);
+
+    System.out.format("The value of pi is %.4f%n", Math.PI);
+    System.out.format("The arccosine of %.4f is %.4f degrees %n", Math.cos(radians),
+        Math.toDegrees(Math.acos(Math.sin(radians))));
+  }
+
+  private static void executeAtan() {
+    double degrees = 45.0;
+    double radians = Math.toRadians(degrees);
+
+    System.out.format("The value of pi is %.4f%n", Math.PI);
+    System.out.format("The arctangent of %.4f is %.4f degrees %n", Math.cos(radians),
+        Math.toDegrees(Math.atan(Math.sin(radians))));
+  }
+
+  private static void executeAtan2() {
+    double x = 45.0;
+    double y = 30.0;
+
+    System.out.println(Math.atan2(x, y));
+  }
+
+  private static void executeToDegrees() {
+    double x = 45.0;
+    double y = 30.0;
+
+    System.out.println(Math.toDegrees(x));
+    System.out.println(Math.toDegrees(y));
+  }
+
+  private static void executeToRadians() {
+    double x = 45.0;
+    double y = 30.0;
+
+    System.out.println(Math.toRadians(x));
+    System.out.println(Math.toRadians(y));
+  }
+
+  private static void executeRandom() {
+    System.out.println(Math.random());
+    System.out.println(Math.random());
+  }
+
+
   public static void main(String args[]) {
     System.out.println("Boxing and Unboxing");
     executeBoxing();
@@ -212,5 +276,21 @@ public class Test {
     executeSin();
     System.out.println("\ncos()");
     executeCos();
+    System.out.println("\ntan()");
+    executeTan();
+    System.out.println("\nasin()");
+    executeAsin();
+    System.out.println("\nacos()");
+    executeAcos();
+    System.out.println("\natan()");
+    executeAtan();
+    System.out.println("\natan2()");
+    executeAtan2();
+    System.out.println("\ntoDegrees()");
+    executeToDegrees();
+    System.out.println("\ntoRadians()");
+    executeToRadians();
+    System.out.println("\nrandom()");
+    executeRandom();
   }
 }
