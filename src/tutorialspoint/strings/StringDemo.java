e49b9ad035a32d92902b25dcfc3e28861b3c79ae
Peti <antalpeti@users.noreply.github.com>
2015-02-21 17:33:30 +0100

    Minor fix. Added examples about contentEquals, copyValueOf, endsWith and equals methods.

--- src/tutorialspoint/strings/StringDemo.java
+++ src/tutorialspoint/strings/StringDemo.java
@@ -45,7 +45,56 @@ public class StringDemo {
     System.out.println(s);
   }
 
+  private static void executeContentEquals() {
+    String str1 = "Not immutable";
+    String str2 = "Strings are immutable";
+    StringBuffer str3 = new StringBuffer("Not immutable");
 
+    boolean result = str1.contentEquals(str3);
+    System.out.println(result);
+
+    result = str2.contentEquals(str3);
+    System.out.println(result);
+  }
+
+  private static void executeCopyValueOf() {
+    char[] Str1 = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+    String Str2 = "";
+
+    Str2 = String.copyValueOf(Str1);
+    System.out.println("Returned String: " + Str2);
+
+    Str2 = String.copyValueOf(Str1, 2, 6);
+    System.out.println("Returned String: " + Str2);
+  }
+
+  private static void executeEndsWith() {
+    String Str = new String("This is really not immutable!!");
+    boolean retVal;
+
+    retVal = Str.endsWith("immutable!!");
+    System.out.println("Returned Value = " + retVal);
+
+    retVal = Str.endsWith("immu");
+    System.out.println("Returned Value = " + retVal);
+  }
+
+  private static void executeEquals() {
+    String Str1 = new String("This is really not immutable!!");
+    String Str2 = Str1;
+    String Str3 = new String("This is really not immutable!!");
+    String Str4 = new String("This is immutable!!");
+    boolean retVal;
+
+    retVal = Str1.equals(Str2);
+    System.out.println("Returned Value = " + retVal);
+
+    retVal = Str1.equals(Str3);
+    System.out.println("Returned Value = " + retVal);
+
+    retVal = Str1.equals(Str4);
+    System.out.println("Returned Value = " + retVal);
+  }
 
   public static void main(String args[]) {
     char[] helloArray = {'h', 'e', 'l', 'l', 'o', '.'};
@@ -72,13 +121,22 @@ public class StringDemo {
             + "variable is %d, and the string " + "is %s", floatVar, intVar, stringVar);
     System.out.println(fs);
 
-    System.out.println("charAt(int index)");
+    System.out.println("\ncharAt(int index)");
     executeCharAt();
-    System.out.println("compareTo(Object o) or compareTo(String anotherString)");
+    System.out.println("\ncompareTo(String anotherString)");
     executeCompareTo();
-    System.out.println("compareToIgnoreCase(String str)");
+    System.out.println("\ncompareToIgnoreCase(String str)");
     executeCompareToIgnoreCase();
-    System.out.println("concat(String s)");
+    System.out.println("\nconcat(String s)");
     executeConcat();
+    System.out.println("\ncontentEquals(StringBuffer sb)");
+    executeContentEquals();
+    System.out.println("\ncopyValueOf(cahr[] data) "
+        + "and copyValueOf(char[] data, int offset, int count)");
+    executeCopyValueOf();
+    System.out.println("\nendsWith(String suffix)");
+    executeEndsWith();
+    System.out.println("\nequals(Object anObject)");
+    executeEquals();
   }
 }
