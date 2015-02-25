1409f8325b484236a31d9e15d1863ab8b83024ce
Peti <antalpeti@users.noreply.github.com>
2015-02-25 22:27:42 +0100

    Added examples about append, reverse, delete, insert and replace methods.

---
+++ src/tutorialspoint/strings/StringBufferBuilderDemo.java
@@ -0,0 +1,57 @@
+package tutorialspoint.strings;
+
+public class StringBufferBuilderDemo {
+
+  private static void executeAppend() {
+    StringBuffer sb = new StringBuffer("Test");
+    sb.append(" String Buffer");
+    System.out.println(sb);
+  }
+
+  private static void executeReverse() {
+    StringBuffer buffer = new StringBuffer("Game Plan");
+    buffer.reverse();
+    System.out.println(buffer);
+  }
+
+  private static void executeDelete() {
+    StringBuffer sb = new StringBuffer("abcdefghijk");
+    sb.delete(3, 7);
+    System.out.println(sb);
+  }
+
+  private static void executeInsert() {
+    StringBuffer sb = new StringBuffer("abcdefghijk");
+    sb.insert(3, "123");
+    System.out.println(sb);
+  }
+
+  private static void executeReplace() {
+    StringBuffer sb = new StringBuffer("abcdefghijk");
+    sb.replace(3, 8, "ZARA");
+    System.out.println(sb);
+  }
+
+  public static void main(String args[]) {
+    StringBuffer sBuffer = new StringBuffer(" test");
+    sBuffer.append(" String Buffer");
+    System.out.println(sBuffer);
+
+    System.out.println("\nappend(String s)");
+    executeAppend();
+
+    System.out.println("\nreverse()");
+    executeReverse();
+
+    System.out.println("\ndelete()");
+    executeDelete();
+
+    System.out.println("\ninsert(int offset, int i)");
+    executeInsert();
+
+    System.out.println("\nreplace(int start, int end, String str)");
+    executeReplace();
+
+
+  }
+}
