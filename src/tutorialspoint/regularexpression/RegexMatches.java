57797511b669e29d1ede21c8d3e279c435386300
Peti <antalpeti@users.noreply.github.com>
2015-03-08 10:38:09 +0100

    Added examples about regular expression metacharacters ^ $ . [...] [^...] \A \z \Z * + ?

--- src/tutorialspoint/regularexpression/RegexMatches.java
+++ src/tutorialspoint/regularexpression/RegexMatches.java
@@ -4,6 +4,59 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexMatches {
+
+  private static void executeRegExMetacharacter() {
+    String line = "This is the simple example";
+    String pattern = "^(.)";
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(line);
+    while (m.find()) {
+      System.out.println("Matches beginning of line: " + m.group(0));
+    }
+    m = Pattern.compile(".$").matcher(line);
+    while (m.find()) {
+      System.out.println("Matches end of line: " + m.group(0));
+    }
+    m = Pattern.compile(".").matcher(line);
+    while (m.find()) {
+      System.out.println("Matches any single character except newline:  " + m.group(0));
+    }
+    m = Pattern.compile("e[xm]a").matcher(line);
+    while (m.find()) {
+      System.out.println("Matches any single character in brackets: " + m.group(0));
+    }
+    m = Pattern.compile("[^h]is").matcher(line);
+    while (m.find()) {
+      System.out.println("Matches any single character not in brackets: " + m.group(0));
+    }
+    m = Pattern.compile("\\A..").matcher(line);
+    while (m.find()) {
+      System.out.println("Beginning of entire string: " + m.group(0));
+    }
+    m = Pattern.compile("..\\z").matcher(line);
+    while (m.find()) {
+      System.out.println("End of entire string: " + m.group(0));
+    }
+    m = Pattern.compile("..\\Z").matcher(line);
+    while (m.find()) {
+      System.out.println("End of entire string except allowable final line terminator: "
+          + m.group(0));
+    }
+    line = "Hello nice little regex nice at all.";
+    m = Pattern.compile("regt*").matcher(line);
+    while (m.find()) {
+      System.out.println("Matches 0 or more occurrences of preceding expression: " + m.group(0));
+    }
+    m = Pattern.compile("nic+").matcher(line);
+    while (m.find()) {
+      System.out.println("Matches 1 or more of the previous thing: " + m.group(0));
+    }
+    m = Pattern.compile("reget?").matcher(line);
+    while (m.find()) {
+      System.out.println("Matches 0 or 1 occurrence of preceding expression: " + m.group(0));
+    }
+  }
+
   public static void main(String args[]) {
 
     // String to be scanned to find the pattern.
@@ -25,5 +78,8 @@ public class RegexMatches {
     } else {
       System.out.println("NO MATCH");
     }
+
+    System.out.println("\nRegluar Expression Metacharacters");
+    executeRegExMetacharacter();
   }
 }
