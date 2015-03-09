53415d8d4ec7f01c39a00efbd74a8a3b2619a134
Peti <antalpeti@users.noreply.github.com>
2015-03-09 17:37:33 +0100

    Added example about finalize method usage.

--- src/tutorialspoint/methods/ConsDemo.java
+++ src/tutorialspoint/methods/ConsDemo.java
@@ -2,7 +2,10 @@ package tutorialspoint.methods;
 
 public class ConsDemo {
 
+  private static Object obj;
+
   public static void main(String args[]) {
+    obj = new Object();
     MyClass t1 = new MyClass();
     MyClass t2 = new MyClass();
     System.out.println(t1.x + " " + t2.x);
@@ -10,5 +13,11 @@ public class ConsDemo {
     MyClass t3 = new MyClass(10);
     MyClass t4 = new MyClass(20);
     System.out.println(t3.x + " " + t4.x);
+  }
+
+  @Override
+  protected void finalize() throws Throwable {
+    super.finalize();
+    obj = null;
   }
 }
