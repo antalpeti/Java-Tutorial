c0c5f4d8472ddb402c3f7938d3b987ed5b96b776
Peti <antalpeti@users.noreply.github.com>
2015-03-15 09:49:50 +0100

    Added Java Keywords inside a html table with css style.

--- src/tutorialspoint/quickguide/package-info.java
+++ src/tutorialspoint/quickguide/package-info.java
@@ -1,4 +1,94 @@
 /**
  * It contains a quick overview to the Java language.
+ *
+ * <h2>Java Keywords:</h2>
+ * <p>
+ * The following list shows the reserved words in Java. These reserved words may not be used as
+ * constant or variable or any other identifier names.
+ * </p>
+ * <style> table { border-collapse: collapse; width: 100%; vertical-align: middle; text-align:
+ * center;} table, td, th { border: 1px solid black; } </style>
+ * <table>
+ * <tbody>
+ * <tr>
+ * <td>abstract</td>
+ * <td>assert</td>
+ * <td>boolean</td>
+ * <td>break</td>
+ * </tr>
+ * <tr>
+ * <td>byte</td>
+ * <td>case</td>
+ * <td>catch</td>
+ * <td>char</td>
+ * </tr>
+ * <tr>
+ * <td>class</td>
+ * <td>const</td>
+ * <td>continue</td>
+ * <td>default</td>
+ * </tr>
+ * <tr>
+ * <td>do</td>
+ * <td>double</td>
+ * <td>else</td>
+ * <td>enum</td>
+ * </tr>
+ * <tr>
+ * <td>extends</td>
+ * <td>final</td>
+ * <td>finally</td>
+ * <td>float</td>
+ * </tr>
+ * <tr>
+ * <td>for</td>
+ * <td>goto</td>
+ * <td>if</td>
+ * <td>implements</td>
+ * </tr>
+ * <tr>
+ * <td>import</td>
+ * <td>instanceof</td>
+ * <td>int</td>
+ * <td>interface</td>
+ * </tr>
+ * <tr>
+ * <td>long</td>
+ * <td>native</td>
+ * <td>new</td>
+ * <td>package</td>
+ * </tr>
+ * <tr>
+ * <td>private</td>
+ * <td>protected</td>
+ * <td>public</td>
+ * <td>return</td>
+ * </tr>
+ * <tr>
+ * <td>short</td>
+ * <td>static</td>
+ * <td>strictfp</td>
+ * <td>super</td>
+ * </tr>
+ * <tr>
+ * <td>switch</td>
+ * <td>synchronized</td>
+ * <td>this</td>
+ * <td>throw</td>
+ * </tr>
+ * <tr>
+ * <td>throws</td>
+ * <td>transient</td>
+ * <td>try</td>
+ * <td>void</td>
+ * </tr>
+ * <tr>
+ * <td>volatile</td>
+ * <td>while</td>
+ * <td></td>
+ * <td></td>
+ * </tr>
+ * </tbody>
+ * </table>
  */
 package tutorialspoint.quickguide;
