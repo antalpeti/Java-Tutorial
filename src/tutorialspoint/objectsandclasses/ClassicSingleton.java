ed2145961599cace346df87041419f1133b0941a
Peti <antalpeti@users.noreply.github.com>
2015-03-17 18:33:53 +0100

    Added example about Singleton with lazy instantation.

---
+++ src/tutorialspoint/objectsandclasses/ClassicSingleton.java
@@ -0,0 +1,17 @@
+package tutorialspoint.objectsandclasses;
+
+public class ClassicSingleton {
+
+  private static ClassicSingleton instance = null;
+
+  protected ClassicSingleton() {
+    // Exists only to defeat instantiation.
+  }
+
+  public static ClassicSingleton getInstance() {
+    if (instance == null) {
+      instance = new ClassicSingleton();
+    }
+    return instance;
+  }
+}
