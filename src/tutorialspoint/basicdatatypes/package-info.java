9049706b362f46f27a0ec0a5b9924b81a783b263
Peti <antalpeti@users.noreply.github.com>
2015-03-17 18:58:05 +0100

    Added package-info file and table of escape sequences.

---
+++ src/tutorialspoint/basicdatatypes/package-info.java
@@ -0,0 +1,67 @@
+/**
+ * Contains some examples about Basic Datatypes topic.
+ *
+ *
+ * <p>
+ * Java language supports few special escape sequences for String and char literals as well. They
+ * are:
+ * </p>
+ * <style> table { border-collapse: collapse; width: 100%; vertical-align: middle; text-align:
+ * center;} table, td, th { border: 1px solid black; } thead {color: green} tbody{color:
+ * blue}</style>
+ * <table>
+ * <table>
+ * <thead>
+ * <tr>
+ * <th>Notation</th>
+ * <th>Character represented</th>
+ * </tr>
+ * </thead> <tbody>
+ * <tr>
+ * <td>\n</td>
+ * <td>Newline (0x0a)</td>
+ * </tr>
+ * <tr>
+ * <td>\r</td>
+ * <td>Carriage return (0x0d)</td>
+ * </tr>
+ * <tr>
+ * <td>\f</td>
+ * <td>Formfeed (0x0c)</td>
+ * </tr>
+ * <tr>
+ * <td>\b</td>
+ * <td>Backspace (0x08)</td>
+ * </tr>
+ * <tr>
+ * <td>\s</td>
+ * <td>Space (0x20)</td>
+ * </tr>
+ * <tr>
+ * <td>\t</td>
+ * <td>tab</td>
+ * </tr>
+ * <tr>
+ * <td>\"</td>
+ * <td>Double quote</td>
+ * </tr>
+ * <tr>
+ * <td>\'</td>
+ * <td>Single quote</td>
+ * </tr>
+ * <tr>
+ * <td>\\</td>
+ * <td>backslash</td>
+ * </tr>
+ * <tr>
+ * <td>\ddd</td>
+ * <td>Octal character (ddd)</td>
+ * </tr>
+ * <tr>
+ * <td>\uabcd</td>
+ * <td>Hexadecimal UNICODE character (xxxx)</td>
+ * </tr>
+ * </tbody>
+ * </table>
+ */
+package tutorialspoint.basicdatatypes;
