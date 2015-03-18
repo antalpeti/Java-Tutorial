5b905900f7d54e72d7cce3491912fef7c1b7d24d
Peti <antalpeti@users.noreply.github.com>
2015-03-18 16:39:13 +0100

    Added picture about class hierarchy of Number class and its subclasses.

--- src/tutorialspoint/numbers/package-info.java
+++ src/tutorialspoint/numbers/package-info.java
@@ -1,4 +1,11 @@
 /**
  * Contains some examples about Numbers topic.
+ *
+ * <p>
+ * All the wrapper classes (Integer, Long, Byte, Double, Float, Short) are subclasses of the
+ * abstract class Number.
+ * </p>
+ *
+ * <img src="./number_classes.jpg" alt="Number Classes" width="100%">
  */
 package tutorialspoint.numbers;
