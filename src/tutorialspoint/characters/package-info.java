085675923d1daf397b983de0bf42045471e8b36d
Peti <antalpeti@users.noreply.github.com>
2015-03-18 17:03:05 +0100

    Added table of escape sequences.

--- src/tutorialspoint/characters/package-info.java
+++ src/tutorialspoint/characters/package-info.java
@@ -1,4 +1,52 @@
 /**
  * Contains some examples about Characters topic.
+ *
+ * <style>table { border: 1px solid #D6D6D6; width: 100%; border-collapse: collapse; vertical-align:
+ * top; margin-top: 8px; margin-bottom: 8px; background-color: #F7F7F7; } tbody { margin: 0px;
+ * padding: 0px; } td { border-collapse: collapse; border: 1px solid #D6D6D6; padding: 5px;
+ * text-align: center } th { border-collapse: collapse; border: 1px solid #D6D6D6; background-color:
+ * #EEE; padding: 5px; text-align: center; }</style>
+ * <p>
+ * <table>
+ * <caption>Escape Sequences</caption> <tbody>
+ * <tr>
+ * <th>Escape Sequence</th>
+ * <th>Description</th>
+ * </tr>
+ * <tr>
+ * <td>\t</td>
+ * <td>Inserts a tab in the text at this point.</td>
+ * </tr>
+ * <tr>
+ * <td>\b</td>
+ * <td>Inserts a backspace in the text at this point.</td>
+ * </tr>
+ * <tr>
+ * <td>\n</td>
+ * <td>Inserts a newline in the text at this point.</td>
+ * </tr>
+ * <tr>
+ * <td>\r</td>
+ * <td>Inserts a carriage return in the text at this point.</td>
+ * </tr>
+ * <tr>
+ * <td>\f</td>
+ * <td>Inserts a form feed in the text at this point.</td>
+ * </tr>
+ * <tr>
+ * <td>\'</td>
+ * <td>Inserts a single quote character in the text at this point.</td>
+ * </tr>
+ * <tr>
+ * <td>\"</td>
+ * <td>Inserts a double quote character in the text at this point.</td>
+ * </tr>
+ * <tr>
+ * <td>\\</td>
+ * <td>Inserts a backslash character in the text at this point.</td>
+ * </tr>
+ * </tbody>
+ * </table>
+ * </p>
  */
 package tutorialspoint.characters;
