214f1e0b7324fc734cec55ba04e2ade0268bc482
Peti <antalpeti@users.noreply.github.com>
2015-03-20 17:01:37 +0100

    Added examples about DataInputStream and DataOutputStream methods. Mainly unclear how it works.

--- src/tutorialspoint/filesandio/DataStreamTest.java
+++ src/tutorialspoint/filesandio/DataStreamTest.java
@@ -22,5 +22,51 @@ public class DataStreamTest {
     }
     d.close();
     out.close();
+    readMethods();
+  }
+
+  private static void readMethods() throws IOException {
+    DataInputStream dis = new DataInputStream(System.in);
+    DataOutputStream dos = new DataOutputStream(new FileOutputStream("./files/Handwrites.txt"));
+    System.out.println("Start typing!");
+    byte[] b = new byte[10];
+    dis.read(b, 4, 6);
+    forEachMethod(b);
+    System.out.println("Skipped bytes: " + dis.skip(10));
+    System.out.println("\nLet's try it again! Shall we? And store it an array.");
+    dos.write(dis.read(b));
+    forEachMethod(b);
+    System.out.println("Type 0 or 1!");
+    boolean typedBoolean = dis.readBoolean();
+    dos.writeBoolean(typedBoolean);
+    dos.flush();
+    System.out.println("Skipped bytes: " + dis.skip(10));
+    System.out.println("You typed: " + typedBoolean);
+    System.out.println("Type a byte number please!");
+    byte typedByte = dis.readByte();
+    dos.writeByte(typedByte);
+    System.out.println("You typed: " + typedByte);
+    System.out.println("Skipped bytes: " + dis.skipBytes(10));
+    System.out.println("Type a short number please!");
+    short typedShort = dis.readShort();
+    dos.writeShort(typedShort);
+    System.out.println("You typed: " + typedShort);
+    System.out.println("Skipped bytes: " + dis.skipBytes(10));
+    System.out.println("Type a int number please!");
+    int typedInt = dis.readInt();
+    dos.writeInt(typedInt);
+    System.out.println("You typed: " + typedInt);
+    System.out.println("Skipped bytes: " + dis.skipBytes(10));
+    dis.close();
+    dos.close();
+  }
+
+  /**
+   * @param b
+   */
+  private static void forEachMethod(byte[] b) {
+    for (byte c : b) {
+      System.out.println((char) c);
+    }
   }
 }
