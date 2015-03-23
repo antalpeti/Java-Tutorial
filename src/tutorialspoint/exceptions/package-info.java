98fb923b1180957cb9977582a3fa980db987e7c4
Peti <antalpeti@users.noreply.github.com>
2015-03-23 09:10:11 +0100

    Added image about the hierarchy of Exeception.

--- src/tutorialspoint/exceptions/package-info.java
+++ src/tutorialspoint/exceptions/package-info.java
@@ -1,4 +1,9 @@
 /**
  * Contains some examples about Exceptions topic.
+ *
+ * <p>
+ * The Exception class has two main subclasses: IOException class and RuntimeException Class.
+ * </p>
+ * <img src="./exceptions.jpg" alt="Exception Hierarchy" width="100%">
  */
 package tutorialspoint.exceptions;
