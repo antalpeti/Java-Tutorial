bed58b23327fae23d84462384f75ed3db1cf10ec
Peti <antalpeti@users.noreply.github.com>
2015-03-23 13:14:39 +0100

    Added example about exception throw, multiple exception declaration after throws and finally block usage.

--- src/tutorialspoint/exceptions/ExcepTest.java
+++ src/tutorialspoint/exceptions/ExcepTest.java
@@ -1,10 +1,13 @@
 package tutorialspoint.exceptions;
 
+import java.io.FileNotFoundException;
+import java.rmi.RemoteException;
+
 public class ExcepTest {
 
   public static void main(String args[]) {
+    int a[] = new int[2];
     try {
-      int a[] = new int[2];
       System.out.println("Access element three :" + a[3]);
     } catch (ArrayIndexOutOfBoundsException e) {
       System.out.println("Exception thrown  :" + e);
@@ -16,7 +19,22 @@ public class ExcepTest {
       System.out.println("toString: " + e.toString());
       System.out.println("printStackTrace: ");
       e.printStackTrace();
+    } finally {
+      a[0] = 6;
+      System.out.println("First element value: " + a[0]);
+      System.out.println("The finally statement is executed");
     }
     System.out.println("Out of the block");
+
+  }
+
+  public void deposit(double amount) throws RemoteException {
+    // Method implementation
+    throw new RemoteException();
+  }
+
+  public void withdraw(double amount) throws RemoteException, FileNotFoundException {
+    // Method implementation
+    throw new FileNotFoundException();
   }
 }
