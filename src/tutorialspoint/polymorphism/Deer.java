8a05678bc4229b30d36312cec2d3c785e2105898
Peti <antalpeti@users.noreply.github.com>
2015-03-29 13:05:23 +0200

    Tried out the final modifier.

--- src/tutorialspoint/polymorphism/Deer.java
+++ src/tutorialspoint/polymorphism/Deer.java
@@ -2,9 +2,11 @@ package tutorialspoint.polymorphism;
 
 public class Deer extends Animal implements Vegetarian {
   public static void main(String[] args) {
-    Deer d = new Deer();
+    final Deer d = new Deer();
     Animal a = d;
     Vegetarian v = d;
     Object o = d;
+    final Animal a1 = new Animal();
+    // a1 = new Deer(); //compile time error
   }
 }
