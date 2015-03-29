8556fb74aeb861020363142e695bf35c0d23fb69
Peti <antalpeti@users.noreply.github.com>
2015-03-29 22:25:18 +0200

    Grammatical fix.

--- src/tutorialspoint/interfaces/NameOfInterface.java
+++ src/tutorialspoint/interfaces/NameOfInterface.java
@@ -4,5 +4,5 @@ package tutorialspoint.interfaces;
 
 public interface NameOfInterface {
   // Any number of final, static fields
-  // Any number of abstract method declarations\
+  // Any number of abstract method declarations
 }
