58668cf1d9e8922401c8d2e4f2ce946e24d9f0b7
Peti <antalpeti@users.noreply.github.com>
2015-03-29 13:55:11 +0200

    Added example about abstract class.

---
+++ src/tutorialspoint/abstraction/AbstractDemo.java
@@ -0,0 +1,8 @@
+package tutorialspoint.abstraction;
+
+public class AbstractDemo {
+  public static void main(String[] args) {
+    /* Following is not allowed and would raise error */
+    // Employee e = new Employee("George W.", "Houston, TX", 43); // Compile time error
+  }
+}
