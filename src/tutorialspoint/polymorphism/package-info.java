4ffaa49ca5e3a4d5ebea24dafb542eb198e02f99
Peti <antalpeti@users.noreply.github.com>
2015-03-30 08:38:59 +0200

    Minor formatting.

--- src/tutorialspoint/polymorphism/package-info.java
+++ src/tutorialspoint/polymorphism/package-info.java
@@ -2,17 +2,17 @@
  * Contains some examples about Polymorphism topic.
  *
  * <p>
- * Any Java object that can pass more than one IS-A test is considered to be polymorphic. In Java,
- * all Java objects are polymorphic since any object will pass the IS-A test for their own type and
- * for the class Object.
+ * Any Java object that can pass more than one <b>IS-A</b> test is considered to be
+ * <b>polymorphic</b>. In Java, all Java objects are polymorphic since any object will pass the IS-A
+ * test for their own type and for the class Object.
  * </p>
  *
  * <p>
  * When at compile time, the compiler used a method() in superclass to validate a statement but run
- * time, the JVM invokes this method() from the subclass. This behavior is referred to as virtual
- * method invocation, and the methods are referred to as virtual methods. All methods in Java behave
- * in this manner, whereby an overridden method is invoked at run time, no matter what data type the
- * reference is that was used in the source code at compile time.
+ * time, the JVM invokes this method() from the subclass. This behavior is referred to as <b>virtual
+ * method invocation</b>, and the methods are referred to as <b>virtual methods</b>. All methods in
+ * Java behave in this manner, whereby an overridden method is invoked at run time, no matter what
+ * data type the reference is that was used in the source code at compile time.
  * </p>
  */
 package tutorialspoint.polymorphism;
