94ace63fbebdadd844e17e5513426ad65043931e
Peti <antalpeti@users.noreply.github.com>
2015-03-31 19:20:49 +0200

    Added description about directory structure of package and CLASSPATH system variable.

--- src/tutorialspoint/packages/package-info.java
+++ src/tutorialspoint/packages/package-info.java
@@ -14,7 +14,7 @@
  * <p>
  * Some of the existing packages in Java are::
  * </p>
- * <ul class="list">
+ * <ul>
  * <li>
  * <p>
  * <b>java.lang</b> - bundles the fundamental classes
@@ -58,6 +58,7 @@
  * </p>
  *
  * <h2>The import Keyword:</h2>
+ *
  * <p>
  * If a class wants to use another class in the same package, the package name does not need to be
  * used. Classes in the same package find each other without any special syntax.
@@ -92,5 +93,178 @@
  * <b>Note:</b> A class file can contain any number of import statements. The import statements must
  * appear after the package statement and before the class declaration.
  * </p>
+ *
+ * <h2>The Directory Structure of Packages:</h2>
+ *
+ * <p>
+ * Two major results occur when a class is placed in a package:
+ * </p>
+ * <ul>
+ * <li>
+ * <p>
+ * The name of the package becomes a part of the name of the class, as we just discussed in the
+ * previous section.
+ * </p>
+ * </li>
+ * <li>
+ * <p>
+ * The name of the package must match the directory structure where the corresponding bytecode
+ * resides.
+ * </p>
+ * </li>
+ * </ul>
+ *
+ * <p>
+ * Here is simple way of managing your files in Java:
+ * </p>
+ *
+ * <p>
+ * Put the source code for a class, interface, enumeration, or annotation type in a text file whose
+ * name is the simple name of the type and whose extension is <b>.java</b>. For example:
+ * </p>
+ *
+ * <pre>
+ * <code>// File Name : Car.java
+ * package vehicle;
+ * 
+ * public class Car {
+ *   // Class implementation.
+ * }</code>
+ * </pre>
+ *
+ * <p>
+ * Now, put the source file in a directory whose name reflects the name of the package to which the
+ * class belongs:
+ * </p>
+ *
+ * <pre>
+ * ....\vehicle\Car.java
+ * </pre>
+ *
+ * <p>
+ * Now, the qualified class name and pathname would be as below:
+ * </p>
+ * <ul>
+ * <li>
+ * <p>
+ * Class name -&gt; vehicle.Car
+ * </p>
+ * </li>
+ * <li>
+ * <p>
+ * Path name -&gt; vehicle\Car.java (in windows)
+ * </p>
+ * </li>
+ * </ul>
+ *
+ * <p>
+ * In general, a company uses its reversed Internet domain name for its package names. Example: A
+ * company's Internet domain name is apple.com, then all its package names would start with
+ * com.apple. Each component of the package name corresponds to a subdirectory.
+ * </p>
+ *
+ * <p>
+ * Example: The company had a com.apple.computers package that contained a Dell.java source file, it
+ * would be contained in a series of subdirectories like this:
+ * </p>
+ *
+ * <pre>
+ * ....\com\apple\computers\Dell.java
+ * </pre>
+ *
+ * <p>
+ * At the time of compilation, the compiler creates a different output file for each class,
+ * interface and enumeration defined in it. The base name of the output file is the name of the
+ * type, and its extension is <b> .class</b>
+ * </p>
+ *
+ * <p>
+ * Like the .java source files, the compiled .class files should be in a series of directories that
+ * reflect the package name. However, the path to the .class files does not have to be the same as
+ * the path to the .java source files. You can arrange your source and class directories separately,
+ * as:
+ * </p>
+ *
+ * <pre>
+ * <path-one>\sources\com\apple\computers\Dell.java
+ * <path-two>\classes\com\apple\computers\Dell.class
+ * </pre>
+ *
+ * <p>
+ * By doing this, it is possible to give the classes directory to other programmers without
+ * revealing your sources. You also need to manage source and class files in this manner so that the
+ * compiler and the Java Virtual Machine (JVM) can find all the types your program uses.
+ * </p>
+ *
+ * <p>
+ * The full path to the classes directory, &lt;path-two&gt;\classes, is called the class path, and
+ * is set with the CLASSPATH system variable. Both the compiler and the JVM construct the path to
+ * your .class files by adding the package name to the class path.
+ * </p>
+ *
+ * <p>
+ * Say &lt;path-two&gt;\classes is the class path, and the package name is com.apple.computers, then
+ * the compiler and JVM will look for .class files in &lt;path-two&gt;\classes\com\apple\compters.
+ * </p>
+ *
+ * <p>
+ * A class path may include several paths. Multiple paths should be separated by a semicolon
+ * (Windows) or colon (Unix). By default, the compiler and the JVM search the current directory and
+ * the JAR file containing the Java platform classes so that these directories are automatically in
+ * the class path.
+ * </p>
+ *
+ * <h2>Set CLASSPATH System Variable:</h2>
+ *
+ * <p>
+ * To display the current CLASSPATH variable, use the following commands in Windows and UNIX (Bourne
+ * shell):
+ * </p>
+ * <ul>
+ * <li>
+ * <p>
+ * In Windows -&gt; C:\&gt; <code>set CLASSPATH</code>
+ * </p>
+ * </li>
+ * <li>
+ * <p>
+ * In UNIX -&gt; <code>% echo $CLASSPATH</code>
+ * </p>
+ * </li>
+ * </ul>
+ *
+ * <p>
+ * To delete the current contents of the CLASSPATH variable, use :
+ * </p>
+ *
+ * <ul>
+ * <li>
+ * <p>
+ * In Windows -&gt; C:\&gt; <code>set CLASSPATH=</code>
+ * </p>
+ * </li>
+ * <li>
+ * <p>
+ * In UNIX -&gt; <code>% unset CLASSPATH; export CLASSPATH</code>
+ * </p>
+ * </li>
+ * </ul>
+ *
+ * <p>
+ * To set the CLASSPATH variable:
+ * </p>
+ *
+ * <ul>
+ * <li>
+ * <p>
+ * In Windows -&gt; <code>set CLASSPATH=C:\<code>users</code>\jack\java\classes</code>
+ * </p>
+ * </li>
+ * <li>
+ * <p>
+ * In UNIX -&gt; % <code>CLASSPATH=/home/jack/java/classes; export CLASSPATH</code>
+ * </p>
+ * </li>
+ * </ul>
  */
 package tutorialspoint.packages;
