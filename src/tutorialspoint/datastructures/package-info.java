b0ca0dd4f70b8da74efa17a8ef5eb93d462d3316
Peti <antalpeti@users.noreply.github.com>
2015-04-01 18:26:06 +0200

    Extended description about the Dictionary class.

--- src/tutorialspoint/datastructures/package-info.java
+++ src/tutorialspoint/datastructures/package-info.java
@@ -145,6 +145,19 @@
  * Since the Dictionary class is abstract, it provides only the framework for a key-mapped data
  * structure rather than a specific implementation.
  * </p>
+ * <p>
+ * Dictionary is an abstract class that represents a key/value storage repository and operates much
+ * like Map.
+ * </p>
+ * <p>
+ * Given a key and value, you can store the value in a Dictionary object. Once the value is stored,
+ * you can retrieve it by using its key. Thus, like a map, a dictionary can be thought of as a list
+ * of key/value pairs.
+ * </p>
+ * <p>
+ * The Dictionary class is obsolete. You should implement the Map interface to obtain key/value
+ * storage functionality.
+ * </p>
  *
  * <h2>The Hashtable</h2>
  * <p>
