6975d822189b1b1b5062d832e9d89918f9e6ef48
Peti <antalpeti@users.noreply.github.com>
2015-04-04 14:14:36 +0200

    Added examples about usage of containsAll, addAll, removeAll, toArray, size, isEmpty, clear and iterator methods according to Collection interface.

--- src/tutorialspoint/collections/CollectionsDemo.java
+++ src/tutorialspoint/collections/CollectionsDemo.java
@@ -1,8 +1,10 @@
 package tutorialspoint.collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +19,20 @@ public class CollectionsDemo {
     a1.add("Ayan");
     System.out.println(" ArrayList Elements");
     System.out.print("\t" + a1);
+    List<String> a2 = new ArrayList<String>();
+    a2.add("Tom");
+    a2.add("Anna");
+    System.out.println("\ncontainsAll(a1): " + a1.containsAll(a1));
+    System.out.println("containsAll(a2): " + a1.containsAll(a2));
+    System.out.println("addAll(a2): " + a1.addAll(a2));
+    System.out.println("containsAll(a2): " + a1.containsAll(a2));
+    System.out.println(" ArrayList Elements");
+    System.out.print("\t" + a1);
+    System.out.println("\nremoveAll(a2): " + a1.removeAll(a2));
+    System.out.println(" ArrayList Elements");
+    System.out.print("\t" + a1);
+    Object[] array = a1.toArray();
+    System.out.println("\ntoArray: " + Arrays.toString(array));
 
     List<String> l1 = new LinkedList<String>();
     l1.add("Zara");
@@ -25,12 +41,31 @@ public class CollectionsDemo {
     System.out.println();
     System.out.println(" LinkedList Elements");
     System.out.print("\t" + l1);
+    System.out.println("\ncontainsAll(l1): " + l1.containsAll(l1));
+    System.out.println("size: " + l1.size());
+    System.out.println("isEmpty: " + l1.isEmpty());
+    l1.clear();
+    System.out.println("size after clear: " + l1.size());
+    System.out.println("isEmpty after clear: " + l1.isEmpty());
 
     Set<String> s1 = new HashSet<String>();
     s1.add("Zara");
     s1.add("Mahnaz");
     s1.add("Ayan");
     System.out.println();
+    System.out.println(" Set Elements");
+    System.out.print("\t" + s1);
+    System.out.println("\ncontainsAll(s1): " + s1.containsAll(s1));
+    Iterator<String> iterator = s1.iterator();
+    System.out.println("Iterate through the collection:");
+    while (iterator.hasNext()) {
+      String string = iterator.next();
+      if (string.equals("Ayan")) {
+        iterator.remove();
+      } else {
+        System.out.println(string);
+      }
+    }
     System.out.println(" Set Elements");
     System.out.print("\t" + s1);
 
@@ -40,7 +75,7 @@ public class CollectionsDemo {
     m1.put("Ayan", "12");
     m1.put("Daisy", "14");
     System.out.println();
-    System.out.println(" Map Elements");
+    System.out.println("\n Map Elements");
     System.out.print("\t" + m1);
   }
 }
