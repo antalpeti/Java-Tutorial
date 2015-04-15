34be8012dacf05cedc91d5980f5cd03fe1cae18e
Peti <antalpeti@users.noreply.github.com>
2015-04-15 15:44:08 +0200

    Added examples about usage of entrySet according to the WeakHashMap class.

--- src/tutorialspoint/collections/WeakHashMapDemo.java
+++ src/tutorialspoint/collections/WeakHashMapDemo.java
@@ -1,6 +1,7 @@
 package tutorialspoint.collections;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.WeakHashMap;
 
 public class WeakHashMapDemo {
@@ -9,6 +10,12 @@ public class WeakHashMapDemo {
   public static void main(String args[]) {
     map = new WeakHashMap<String, String>();
     map.put(new String("Maine"), "Augusta");
+    map.put(new String("Tom"), "Hassel");
+    map.put(new String("Marry"), "Popkins");
+    System.out.println("Content of the WeakHashMap: " + map);
+    for (Entry<String, String> entry : map.entrySet()) {
+      System.out.println("key:" + entry.getKey() + " value: " + entry.getValue());
+    }
 
     Runnable runner = new Runnable() {
       @Override
