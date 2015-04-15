8530d38bdde09d0f5a4ae672e9a93a61794c72c4
Peti <antalpeti@users.noreply.github.com>
2015-04-15 15:33:21 +0200

    Added example about garbage collection according to the WeakHashMap class.

---
+++ src/tutorialspoint/collections/WeakHashMapDemo.java
@@ -0,0 +1,34 @@
+package tutorialspoint.collections;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class WeakHashMapDemo {
+  private static Map<String, String> map;
+
+  public static void main(String args[]) {
+    map = new WeakHashMap<String, String>();
+    map.put(new String("Maine"), "Augusta");
+
+    Runnable runner = new Runnable() {
+      @Override
+      public void run() {
+        while (map.containsKey("Maine")) {
+          try {
+            Thread.sleep(500);
+          } catch (InterruptedException ignored) {
+          }
+          System.out.println("Thread waiting");
+          System.gc();
+        }
+      }
+    };
+    Thread t = new Thread(runner);
+    t.start();
+    System.out.println("Main waiting");
+    try {
+      t.join();
+    } catch (InterruptedException ignored) {
+    }
+  }
+}
