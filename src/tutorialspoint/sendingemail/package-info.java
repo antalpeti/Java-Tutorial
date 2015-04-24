364f7cdf02b2b1c0dd096fb59be0273d07e21557
Peti <antalpeti@users.noreply.github.com>
2015-04-24 16:47:01 +0200

    Extended description about the JavaMail API and the Java Activation Framework (JAF).

--- src/tutorialspoint/sendingemail/package-info.java
+++ src/tutorialspoint/sendingemail/package-info.java
@@ -1,4 +1,25 @@
 /**
  * Contains some examples about Sending Email topic.
+ *
+ * <p>
+ * To send an e-mail using your Java Application is simple enough but to start with you should have
+ * <b>JavaMail API</b> and <b>Java Activation Framework (JAF)</b> installed on your machine.
+ * </p>
+ * <ul>
+ * <li>
+ * <p>
+ * You can download latest version of <a
+ * href="http://www.oracle.com/technetwork/java/index-138643.html" target="_blank">JavaMail (Version
+ * 1.4.5</a> from Java's standard website.
+ * </p>
+ * </li>
+ * <li>
+ * <p>
+ * You can download latest version of <a
+ * href="http://www.oracle.com/technetwork/articles/java/index-135046.html" target="_blank">JAF
+ * (Version 1.1.1)</a> from Java's standard website.
+ * </p>
+ * </li>
+ * </ul>
  */
 package tutorialspoint.sendingemail;
