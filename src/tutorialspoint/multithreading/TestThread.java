7621026a796cf0ca310f07fa5772ae964a34fa60
Peti <antalpeti@users.noreply.github.com>
2015-04-29 23:20:56 +0200

    Added example about creating thread by extending Thread class.

--- src/tutorialspoint/multithreading/TestThread.java
+++ src/tutorialspoint/multithreading/TestThread.java
@@ -8,5 +8,11 @@ public class TestThread {
 
     RunnableDemo R2 = new RunnableDemo("Thread-2");
     R2.start();
+
+    ThreadDemo T1 = new ThreadDemo("Thread-3");
+    T1.start();
+
+    ThreadDemo T2 = new ThreadDemo("Thread-4");
+    T2.start();
   }
 }
