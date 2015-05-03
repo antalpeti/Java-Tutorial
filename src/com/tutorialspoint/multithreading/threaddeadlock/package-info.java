ca4de130917af485ba76e1e74fb80c331430449b
Peti <antalpeti@users.noreply.github.com>
2015-05-03 07:43:38 +0200

    Extended package-info description.

--- src/com/tutorialspoint/multithreading/threaddeadlock/package-info.java
+++ src/com/tutorialspoint/multithreading/threaddeadlock/package-info.java
@@ -1,4 +1,12 @@
 /**
  * Contains some examples about Thread Deadlock topic.
+ *
+ * <p>
+ * Deadlock describes a situation where two or more threads are blocked forever, waiting for each
+ * other. Deadlock occurs when multiple threads need the same locks but obtain them in different
+ * order. A Java multithreaded program may suffer from the deadlock condition because the
+ * <b>synchronized</b> keyword causes the executing thread to block while waiting for the lock, or
+ * monitor, associated with the specified object.
+ * </p>
  */
 package com.tutorialspoint.multithreading.threaddeadlock;
