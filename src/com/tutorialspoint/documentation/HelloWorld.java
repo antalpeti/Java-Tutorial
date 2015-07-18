b05cd396686da98e13f61ea4ec1d1fa5e6a7a970
Peti <antalpeti@users.noreply.github.com>
2015-07-18 20:02:56 +0200

    Add example about the usage of <h1> and <p> tags.

--- src/com/tutorialspoint/documentation/HelloWorld.java
+++ src/com/tutorialspoint/documentation/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.tutorialspoint.documentation;
 
 /**
- * The HelloWorld program implements an application that simply displays "Hello World!" to the
- * standard output.
+ * <h1>Hello, World!</h1> The HelloWorld program implements an application that simply displays
+ * "Hello World!" to the standard output.
+ * <p>
+ * Giving proper comments in your program makes it more user friendly and it is assumed as a high
+ * quality code.
+ * </p>
  *
  * @author Peter
  * @version 1.0
