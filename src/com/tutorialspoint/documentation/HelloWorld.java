cf7b905df14f1773a3a72b307fa4ed1994aa278b
Peti <antalpeti@users.noreply.github.com>
2015-07-18 19:58:52 +0200

    Add example about the usage of @author, @version and @since tags.

---
+++ src/com/tutorialspoint/documentation/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.tutorialspoint.documentation;
+
+/**
+ * The HelloWorld program implements an application that simply displays "Hello World!" to the
+ * standard output.
+ *
+ * @author Peter
+ * @version 1.0
+ * @since 2015-07-18
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    // Prints Hello, World! on standard output.
+    System.out.println("Hello World!");
+  }
+}
